d07ace7fc50f30f97f7ff98b77bf3cdde684c5fd
Jordan ERNST <pro.ernst@gmail.com>
2021-11-26 15:56:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,81 @@
+# Overview
+
+This scripts aims in simplifing patching and installing Magisk after a LineageOS **for microG** update.
+
+It has been made to run on Linux x86 or x64 and patch a boot image for an ARM device.
+
+It detects LineageOS **for microG** version via adb, download the ROM, extracts `payload.bin` and dumps `boot.img`, patches it with Magisk, and flashes it.
+
+:warning: **This has only been tested on a FP3 device, at the time of writing with LineageOS for microG 18.1 (November 18, 2021 build) Magisk v23.0** :warning:
+
+## Run on other device
+
+Before to run this script for an other device, please make sure you understand what you run and every step of the script.
+**I won't be responsible if anything goes wrong.**
+
+The script is designed to:
+ * run the script `boot_patch.sh boot.img` with KEEPVERITY and KEEPFORCEENCRYPT to true
+ * install Magisk by patching the **boot image** and **not the recovery image**.
+ * run to flash the boot image in **boot_a** or **boot_b** partition (the FP3 way) and **not boot partition**
+
+
+If you fit the conditions above, you have to make sure the patch `KEEPVERITY=true KEEPFORCEENCRYPT=true sh boot_patch.sh` outputs the same boot image than the Magisk Manager app:
+ * patch boot via Magisk Manager app
+ * run `boot_patch.sh boot.img` manually
+ * compare both files `magisk_patched-*.img` and `new-boot.img` with diff or by comparing hashes.
+
+ If **and only if** both output files are the same and you meet all the conditions, you can try the python script.
+
+# Prerequisites
+
+## In PATH
+
+You need to have on your system (in your PATH):
+ * adb
+ * fastboot
+ * payload-dumper-go
+ * python (3)
+ * dos2unix
+
+
+## USB debugging
+
+You need to have USB debugging Enabled and configured.
+Check your devices appears when running `adb devices` with device connected via USB.
+
+## Using
+
+Replace the `device` variable by your device code, as [listed here](https://download.lineage.microg.org/).
+
+Run `python magisk_boot_flasher.py`
+
+
+# Run patch boot locally
+
+That's how we can run Magisk's `boot_patch.sh` on Linux x86 or x64 and patch a boot image for an ARM device
+
+> Tested with Magisk v23.0 with FP3
+
+* Get Magisk `.apk`
+* Extract it
+
+Keep in the same folder:
+* `assets/boot_patch.sh` -> `boot_patch.sh`
+* `assets/util_functions.sh` -> `util_functions.sh`
+* `lib/armeabi-v7a/libmagisk32.so` -> `magisk32`
+* `lib/armeabi-v7a/libmagisk64.so` -> `magisk64`
+* `lib/armeabi-v7a/libmagiskinit.so` -> `magiskinit`
+* `lib/x86/libmagiskboot.so` -> `magiskboot`
+* `lib/armeabi-v7a/libmagiskinit.so` -> `magiskinit`
+
+You can delete all the rest.
+
+In util_functions.sh:
+* Change function `ui_print()` to contain only `echo "$1"`
+* Change every `getprop` command, to `adb shell getprop`, to go run it on device rather than locally.
+
+You can now run:
+`sh boot_patch.sh boot.img`
+
+The outpout file `new-boot.img` has the exact same sha256 hash as the `magisk_patched-*.img`, so we can consider it works :)
+
---
+++ magisk_boot_flasher.py
@@ -0,0 +1,201 @@
+import requests
+import hashlib
+import sys
+import zipfile
+from tqdm import tqdm
+import subprocess
+import time
+
+
+device = 'FP3'
+magiskdir = 'Magisk-v23.0'
+
+
+def yes_or_no(question, default=None):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+            It must be "yes" (the default), "no" or None (meaning
+            an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("Invalid default answer: '%s'" % default)
+
+    while True:
+        choice = input(question + prompt).lower()
+        if default is not None and choice == "":
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            print("Please respond with 'yes' or 'no' " "(or 'y' or 'n').\n")
+
+
+def isMagiskInstalled():
+    cmdlist = ['adb', 'shell', 'command', '-v', 'su']
+    sp = subprocess.run(cmdlist, stdout=subprocess.DEVNULL,
+                        stderr=subprocess.STDOUT)
+    if sp.returncode != 0:
+        return False
+    else:
+        return True
+
+
+def checkInstalledVersion():
+    cmdlist = ['adb', 'shell', 'getprop', 'ro.lineage.version']
+    sp = subprocess.run(cmdlist, stdout=subprocess.PIPE)
+    currentversion = sp.stdout.decode('UTF-8').rstrip()
+    return currentversion
+
+
+def downloadUpdate(device, version):
+    filename = f"lineage-{version}.zip"
+    url = f'https://download.lineage.microg.org/{device}/{filename}'
+    r = requests.get(url, stream=True)
+    with tqdm.wrapattr(open(filename, "wb"), "write",
+                       miniters=1, desc=filename,
+                       total=int(r.headers.get('Content-Length'))) as fout:
+        for chunk in r.iter_content(chunk_size=4096):
+            fout.write(chunk)
+
+    print('Comparing hashes...', end='', flush=True)
+    checkHash(filename)
+    print('  [OK]')
+
+    return filename
+
+
+def checkHash(filename):
+    filehash = sha256sum(filename)
+
+    hurl = f'https://download.lineage.microg.org/{device}/{filename}.sha256sum'
+    r = requests.get(hurl)
+    correcthash = r.text.split(' ')[0]
+
+    if filehash != correcthash:
+        sys.exit("Error: File hash doesn't match ! Aborting.")
+
+
+def sha256sum(filename, block_size=65536):
+    sha256 = hashlib.sha256()
+    with open(filename, 'rb') as f:
+        for block in iter(lambda: f.read(block_size), b''):
+            sha256.update(block)
+    return sha256.hexdigest()
+
+
+def patchBootimg():
+    cmd = ['sh', f'{magiskdir}/boot_patch.sh', '../boot.img']
+    env = {"KEEPVERITY": "true", "KEEPFORCEENCRYPT": "true"}
+    sp = subprocess.run(cmd, env=env, stdout=subprocess.DEVNULL,
+                        stderr=subprocess.STDOUT)
+    if sp.returncode != 0:
+        sys.exit('\nERROR: boot_patch.sh exited with errors!')
+
+
+def rebootToBootloader():
+    cmd = ['adb', 'reboot', 'bootloader']
+    subprocess.run(cmd, stdout=subprocess.DEVNULL)
+
+    cmd = ['fastboot', 'devices']
+    while "Waiting for device in fastboot":
+        time.sleep(2)
+        sp = subprocess.run(cmd, stdout=subprocess.PIPE)
+        if sp.stdout != b'':
+            break
+
+
+def flashBoot():
+    currentSlot = getCurrentSlot()
+    cmd = ['fastboot', 'flash', f'boot_{currentSlot}',
+           f'{magiskdir}/new-boot.img']
+    subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+
+
+def getCurrentSlot():
+    cmd = ['fastboot', 'getvar', 'current-slot']
+    ps = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    currentSlot = ps.stdout.decode('UTF-8').rstrip()
+    currentSlot = currentSlot.split('slot: ')[1].split('\n')[0]
+    return currentSlot
+
+
+def cleanUp():
+    cmd = ['rm', 'payload.bin', 'boot.img']
+    subprocess.run(cmd, stdout=subprocess.DEVNULL)
+
+    cmd = ['rm', 'lineage-*.zip']
+    subprocess.run(cmd, shell=True, stdout=subprocess.DEVNULL)
+
+    magiskFilelist = ['kernel', 'kernel_dtb', 'ramdisk.cpio', 'new-boot.img',
+                      'stock_boot.img']
+    for f in magiskFilelist:
+        cmd = ['rm', f'{magiskdir}/{f}']
+        subprocess.run(cmd, stdout=subprocess.DEVNULL)
+
+
+if __name__ == "__main__":
+    print("Checking if Magisk already installed...", end='', flush=True)
+    if isMagiskInstalled():
+        print("\nIt seems Magisk is already installed (su in PATH).")
+        print("Aborting.")
+        sys.exit()
+    print('  [OK]')
+
+    print("Getting installed verion (via adb)...", end='', flush=True)
+    installedversion = checkInstalledVersion()
+    print('  [OK]')
+
+    print("Seems you have haven't flashed yet Magisk on the new firmaware.")
+    if not yes_or_no("Would you like do download the new firmware?"):
+        sys.exit()
+
+    print(f"Downloading version: {installedversion}")
+    filename = downloadUpdate(device, installedversion)
+
+    print("Download finished, extracting payload.bin from release...", end='',
+          flush=True)
+    with zipfile.ZipFile(filename, 'r') as zip_ref:
+        zip_ref.extract('payload.bin')
+    print('  [OK]')
+
+    print("Extracting boot.img...", end='', flush=True)
+    cmd = ['payload-dumper-go', '-p', 'boot', '-o', '.', 'payload.bin']
+    subprocess.run(cmd, stdout=subprocess.DEVNULL)
+    print('  [OK]')
+
+    print("Patching boot.img with Magisk scripts...", end='', flush=True)
+    patchBootimg()
+    print('  [OK]')
+
+    if not yes_or_no("Would you like to flash the new boot image?"):
+        sys.exit()
+
+    print("Make sure your device is connected via USB.")
+    input("Press Enter to continue...")
+
+    print('Rebooting device to bootloader...', end='', flush=True)
+    rebootToBootloader()
+    print('  [OK]')
+
+    print('Flashing new boot image...', end='', flush=True)
+    flashBoot()
+    print('  [OK]')
+
+    print('Cleaning up files...', end='', flush=True)
+    cleanUp()
+    print('  [OK]')
+
+    print('Rebooting device, enjoy Magisk! ;)')
+    cmd = ['fastboot', 'reboot']
+    subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
